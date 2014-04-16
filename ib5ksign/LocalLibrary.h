7a524a1a13c4ccb2297d979795585e55e244a5ea
Sam Galison <samface@gmail.com>
2014-04-16 14:57:07 -0400

    Initial Commit

---
+++ ib5ksign/LocalLibrary.h
@@ -0,0 +1,55 @@
+//
+// File			LocalLibrary.h
+// Brief		Library header
+//
+// Project	 	ib5ksign
+// Developed with [embedXcode](http://embedXcode.weebly.com)
+// 
+// Author		Sam Galison
+// 				Sam Galison
+// Date			4/16/14 2:57 PM
+// Version		<#version#>
+// 
+// Copyright	© Sam Galison, 2014
+// License		<#license#>
+//
+// See			ReadMe.txt for references
+//
+
+
+// Core library - IDE-based
+#if defined(WIRING) // Wiring specific
+#include "Wiring.h"
+#elif defined(MAPLE_IDE) // Maple specific
+#include "WProgram.h"
+#elif defined(MPIDE) // chipKIT specific
+#include "WProgram.h"
+#elif defined(DIGISPARK) // Digispark specific
+#include "Arduino.h"
+#elif defined(ENERGIA) // LaunchPad MSP430 G2 and F5529, Stellaris and Tiva, Experimeter Board FR5739 specific
+#include "Energia.h"
+#elif defined(MICRODUINO) // Microduino specific
+#include "Arduino.h"
+#elif defined(TEENSYDUINO) // Teensy specific
+#include "Arduino.h"
+#elif defined(ARDUINO) // Arduino 1.0 and 1.5 specific
+#include "Arduino.h"
+#else // error
+#error Platform not defined
+#endif // end IDE
+
+#ifndef ib5ksign_LocalLibrary_h
+#define ib5ksign_LocalLibrary_h
+
+//
+// Brief	Blink a LED
+// Details	LED attached to pin is light on then light off
+// Total cycle duration = ms
+// Parameters:
+//      pin pin to which the LED is attached
+//      times number of times
+//      ms cycle duration in ms
+//
+void blink(uint8_t pin, uint8_t times, uint16_t ms);
+
+#endif
---
+++ ib5ksign/ReadMe.txt
@@ -0,0 +1,26 @@
+
+  ib5ksign
+  Project
+  ----------------------------------
+  Developed with embedXcode
+
+  Project ib5ksign
+  Created by Sam Galison on 4/16/14
+  Copyright © 2014 Sam Galison
+  License <#license#>
+
+
+
+  References
+  ----------------------------------
+
+
+
+  embedXcode
+  embedXcode+
+  ----------------------------------
+  Embedded Computing on Xcode
+  Copyright © Rei VILO, 2010-2014
+  All rights reserved
+  http://embedXcode.weebly.com
+
